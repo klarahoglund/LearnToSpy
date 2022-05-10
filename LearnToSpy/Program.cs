fc4114ebcb5fb647c0ce24085c7ced666dd23e23
Admin <Admin@DESKTOP-AP62PKN>
2022-05-10 14:26:54 +0200

    Add project files.

---
+++ LearnToSpy/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Welcome to learn to Spy!");
+
+
+
+
+DateTime hireDate = new DateTime(2022, 05, 01, 08, 30, 0);
+Console.WriteLine("Hire date: " + hireDate);
+
+DateTime exitDate = new DateTime( 2023,05, 05 );
+Console.WriteLine("ExitDate: " + exitDate);
+
+DateTime startDate = hireDate.AddDays(14);
+Console.WriteLine("StartDate: " + startDate);    
+
